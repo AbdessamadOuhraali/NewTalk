efce67a415fc086a308bb1b23b76803b8df4bed2
AbdessamadOuhraali <62210295+AbdessamadOuhraali@users.noreply.github.com>
2021-11-14 09:28:01 +0000

    --

--- lib/main.dart
+++ lib/main.dart
@@ -9,23 +9,61 @@ void main() => runApp(MaterialApp(
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('VEOLKOL'),
-        centerTitle: true,
-        backgroundColor: Colors.amber,
-      ),
-      body: Container(
-          color: Colors.grey,
-          child: Text('pwoirppoeiroi'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(
-          Icons.airplane_ticket,
-          color: Colors.black,
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('WhatsApp'),
+          backgroundColor: Colors.teal[800],
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                // do something
+              },
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.more_vert,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                // do something
+              },
+            ),
+          ],
+          bottom: TabBar(
+            tabs: const [
+              Tab(
+                icon: Icon(Icons.camera_alt),
+              ),
+              Tab(
+                child: Text("CHATS"),
+              ),
+              Tab(
+                  child: Text(
+                "STATUS",
+              )),
+              Tab(
+                  child: Text(
+                "CALLS",
+              )),
+            ],
+            indicatorColor: Colors.white,
+          ),
         ),
-        backgroundColor: Colors.amber,
+        body: Container(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+          backgroundColor: Colors.tealAccent[400],
+        ),
       ),
     );
   }
